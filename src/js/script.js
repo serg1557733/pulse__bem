705893d97b41dfd5dca61db54acc5291166f5b5c
serg1557733 <serg1557733@ukr.net>
2021-05-22 13:54:44 +0300

    tns-slider one of variants

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: true,
         speed: 1100,
         autoplay: true,
         autoplaySpeed: 2000,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/left-solid.png" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/right-solid.png" alt=""></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/left-solid.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right-solid.png" alt=""></button>',
         responsive: [
             {
               breakpoint: 992,
@@ -17,4 +17,14 @@ $(document).ready(function(){
             }
         ]
       });
+  }); */
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controlsText: [
+      '<img src="img/icons/left-solid.png" alt="">','<img src="img/icons/right-solid.png" alt="">'
+    ]
   });
