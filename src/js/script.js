086fd2c9f2fb56af838fda1a59ee4aef267d8006
serg1557733 <serg1557733@ukr.net>
2021-05-23 12:33:00 +0300

    tabs first step

--- src/js/script.js
+++ src/js/script.js
@@ -39,3 +39,15 @@ const slider = tns({
     }
   });
 
+  (function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+      
+    });
+    })(jQuery);
+
